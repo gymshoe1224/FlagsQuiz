e7fae83a75520508f59fb657d43993b5b1e2c479
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-01-09 09:49:07 -0600

    Initial Commit

---
+++ FlagsQuiz/FlagsQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsQuizApp.swift
+//  FlagsQuiz
+//
+//  Created by Chris Markiewicz on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagsQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
