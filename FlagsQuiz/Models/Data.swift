fb7d4d4418189a60fb63b98b509223b58e63b8ba
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-01-18 09:34:12 -0600

    Added more countries to quiz

--- FlagsQuiz/Models/Data.swift
+++ FlagsQuiz/Models/Data.swift
@@ -8,31 +8,23 @@
 import Foundation
 
 struct Data {
-    let questions = [
-        Question(correctAnswer:
-                Answer(text: "England", isCorrect: true),
-             incorrectAnswers: [
-                Answer(text: "Ireland", isCorrect: false),
-                Answer(text: "Iceland", isCorrect: false),
-                Answer(text: "South Africa", isCorrect: false)
-             ])
-        ,
-    
-        Question(correctAnswer:
-                    Answer(text: "France", isCorrect: true),
-                 incorrectAnswers: [
-                    Answer(text: "Spain", isCorrect: false),
-                    Answer(text: "Italy", isCorrect: false),
-                    Answer(text: "Germany", isCorrect: false)
-             ])
-        ,
-    
-        Question(correctAnswer:
-                    Answer(text: "Germany", isCorrect: true),
-                 incorrectAnswers: [
-                    Answer(text: "Austria", isCorrect: false),
-                    Answer(text: "Italy", isCorrect: false),
-                    Answer(text: "Czech Republic", isCorrect: false)
-             ])
-    ]
+    var questions = [Question]()
+    var countries = ["Angola", "Austria", "China", "Egypt", "England", "Estonia", "France", "Germany", "Ghana", "Greece", "India", "Israel", "Italy", "Norway", "Pakistan", "Peru", "Poland", "Portugal", "Qatar", "Saudi Arabia", "South Africa", "Spain", "Sweden", "Ukraine", "United States"]
+    init() {
+        for i in 0..<countries.count {
+            var falseCountries = countries
+            falseCountries.remove(at: falseCountries.firstIndex(of: countries[i])!)
+            let threeFalseCountries = falseCountries.prefix(3)
+            questions.append(
+                Question(
+                    correctAnswer:
+                        Answer(text: countries[i], isCorrect: true),
+                    incorrectAnswers: [
+                        Answer(text: String(threeFalseCountries[0]), isCorrect: false),
+                        Answer(text: String(threeFalseCountries[1]), isCorrect: false),
+                        Answer(text: String(threeFalseCountries[2]), isCorrect: false)]
+                )
+            )
+        }
+    }
 }
